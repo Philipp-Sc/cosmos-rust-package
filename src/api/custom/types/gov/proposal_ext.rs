57d5e815910ba1a25d1b2becbc4460e07fc10b9e
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-04-11 21:07:19 +0700

    fix

--- src/api/custom/types/gov/proposal_ext.rs
+++ src/api/custom/types/gov/proposal_ext.rs
@@ -145,6 +145,15 @@ impl ProposalExt {
             proposal: ProtoMessageWrapper(proposal),
         }
     }
+
+    pub fn get_unique_proposal_id(&self) -> u64 {
+        let mut s = DefaultHasher::new();
+        &self.blockchain.hash(&mut s);
+        &self.status.hash(&mut s);
+        &self.get_proposal_id().hash(&mut s);
+        s.finish()
+    }
+
     pub fn object_to_hash(&self) -> u64 {
         let mut s = DefaultHasher::new();
         &self.hash(&mut s);
